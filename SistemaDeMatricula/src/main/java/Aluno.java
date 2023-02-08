32d064804704c9e437b9b770ca753900c3b52675
Vinicius R <79542676+nivuciis@users.noreply.github.com>
2023-02-08 16:44:56 -0300

    Update Aluno.java

--- SistemaDeMatricula/src/main/java/Aluno.java
+++ SistemaDeMatricula/src/main/java/Aluno.java
@@ -1,4 +1,4 @@
-/*
+*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -10,37 +10,49 @@
 public class Aluno{
 
     public String nome;
-    public int nMatricula;
+    public String nMatricula;
     public int faltas;
     public double notas;
     public String[] disciplinas;
     public String cpf;
     public String Ldisciplinas;
-    public Aluno(String nome, int nMatricula, String[] disciplinas, String cpf)
+    public Aluno()
     {
-        this.nome = nome;
-        this.nMatricula = nMatricula;
-        this.disciplinas = disciplinas;
+        this.nome ;
+        this.nMatricula;
+        this.disciplinas;
         this.faltas = 0;
         this.notas = 0;
-        this.cpf = cpf;
+        this.cpf;
     }
-    public String MostrarInfo()
+    public Aluno InserirAluno(String nome, String nMatricula,String[] disciplinas,String cpf)
     {
-        for(int i=0; i< this.disciplinas.length;i++)
+        a.this.nome = nome;
+        a.this.nMatricula = nMatricula;
+        a.this.disciplinas = disciplinas;
+        a.this.cpf = cpf;
+    }
+    public String MostrarInfo(Aluno a)
+    {
+        for(int i=0; i< a.this.disciplinas.length;i++)
         {
-            this.Ldisciplinas += this.disciplinas[i] + "\n";
+            a.this.Ldisciplinas += a.this.disciplinas[i] + "\n";
         }
-        return "Nome: " + this.nome +
-                "\nNúmero de Matricula: "+ this.nMatricula +
-                "\nNotas: " + this.notas +
-                "\nFaltas: "+ this.faltas +
-                "\nCPF: " + this.cpf + 
-                "\nDisciplinas matriculadas: " + this.Ldisciplinas ; 
+        return "Nome: " + a.this.nome +
+                "\nNúmero de Matricula: "+ a.this.nMatricula +
+                "\nNotas: " + a.this.notas +
+                "\nFaltas: "+ a.this.faltas +
+                "\nCPF: " + a.this.cpf + 
+                "\nDisciplinas matriculadas: " + a.this.Ldisciplinas ;                  
                 
-                
-                             
-                
+    }
+    public Aluno AddFalta(Aluno a)
+    {
+        a.this.faltas += 1;
+    }
+    public Aluno AddNotas(Aluno a, double nota)
+    {
+        a.this.notas += nota;
     }
     
 }
