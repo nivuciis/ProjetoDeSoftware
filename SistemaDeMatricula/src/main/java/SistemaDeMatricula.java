b24a0a39fa8393f05af4279e98ca6e275e1e11a9
Vinicius R <79542676+nivuciis@users.noreply.github.com>
2023-02-08 17:07:12 -0300

    Update SistemaDeMatricula.java

--- SistemaDeMatricula/src/main/java/SistemaDeMatricula.java
+++ SistemaDeMatricula/src/main/java/SistemaDeMatricula.java
@@ -7,20 +7,14 @@
  * @author vinic
 */
 import java.util.Scanner;
-import java.util.ArrayList;
         
 public class SistemaDeMatricula {
 
     public static void main(String[] args) {
-        ArrayList<String> nomes = new ArrayList<String>();
-        ArrayList<Aluno> alunos = new ArrayList<Aluno>();
-        System.out.println("Seja bem vindo ao Sistema de matricula !");
+        System.out.println("Seja bem vindo ao Ganesha !");
         System.out.println("Opções:"
                 + "\n 1.Matricular um aluno no sistema"
-                + "\n 2.Mostrar informações")
-                + "\n 3.Adicionar Falta")
-                + "\n 4.Atribuir nota");
-        Aluno al = new Aluno();
+                + "\n 2.Excluir um aluno do sistema");
         Scanner opt = new Scanner(System.in);
         int options = opt.nextInt();
         switch(options)
@@ -29,7 +23,6 @@ public class SistemaDeMatricula {
                 Scanner matricula = new Scanner(System.in);
                 System.out.println("Digite o nome do aluno");
                 String nome = matricula.nextLine();
-                nomes.add(nome);
                 System.out.println("Digite o numero de matricula");
                 int mat = matricula.nextInt(); matricula.nextLine();
                 System.out.println("Digite as disciplinas matriculadas");
@@ -40,16 +33,9 @@ public class SistemaDeMatricula {
                 }
                 System.out.println("Digite o CPF");
                 String cpf = matricula.nextLine();
-                al.InserirAluno(nome,mat,disciplinas, cpf);
+                Aluno x = new Aluno(nome,mat,disciplinas, cpf);
                 System.out.println();
-                
-            
-            case 2:
-                for(int i=0; i < nomes.size(); i++)
-                {
-                    System.out.println(i+")"+nomes.get(i));
-                }
-                
+                System.out.println(x.MostrarInfo());
         }
     }
 }
